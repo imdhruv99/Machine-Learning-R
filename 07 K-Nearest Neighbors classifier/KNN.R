48973eeb77e013557be1213c53485241e082667d
Dhruv Prajapati <60540925+imdhruv99@users.noreply.github.com>
2021-01-22 21:49:42 +0530

    Added KNN

--- 07 K-Nearest Neighbors classifier/KNN.R
+++ 07 K-Nearest Neighbors classifier/KNN.R
@@ -8,7 +8,7 @@ View(df)
 # setting seed
 set.seed(0)
 
-# spliting data
+# splitting data
 split <- sample.split(df, SplitRatio = 0.8)
 
 # training and testing dataset
@@ -31,3 +31,32 @@ table(test.pred, test_set$Sold)
 #test.pred  0  1
 #      NO  42 16
 #      YES 26 36
+
+# --- K-Nearest Neighbors --- #
+
+# importing class library
+library(class)
+
+trainX <- train_set[, -16]
+testX <- train_set[, -16]
+trainY <- train_set$Sold
+testY <- train_set$Sold
+
+# change the k value and run analysis again
+k <- 3
+
+# standardizing data
+trainX_s <- scale(trainX)
+testX_s <- scale(testX)
+
+# setting seed
+set.seed()
+
+knn.pred <- knn(trainX_s, testX_s, trainY, k = k)
+
+# confusion matrix
+table(knn.pred, testY)
+# knn.pred   0   1
+#        0 162  36
+#        1  46 142
+
